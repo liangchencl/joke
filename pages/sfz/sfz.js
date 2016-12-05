ecd372fbef5cce29888dbe14a32c5777cb35d134
凉城 <627150799@qq.com>
2016-12-05 18:07:19 +0800

    03

--- pages/sfz/sfz.js
+++ pages/sfz/sfz.js
@@ -17,8 +17,10 @@ Page({
       },
       method: 'GET', 
       success: function(res){
+        // 这里还要进行判断，看看输入的值是不是身份证号码。如果不是，应该有提示错误的的！
         console.log(res.data)
         that.setData({
+          sex:res.data.result.sex,
           area:res.data.result.area,
           birthday:res.data.result.birthday
         })
