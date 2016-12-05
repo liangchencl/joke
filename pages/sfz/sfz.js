10c9791a918dc435a689777f40e6941e6e470a0e
凉城 <627150799@qq.com>
2016-12-05 18:02:05 +0800

    03

--- pages/sfz/sfz.js
+++ pages/sfz/sfz.js
@@ -6,17 +6,22 @@ Page({
   },
   //事件处理函数
   bindViewS:function(e){
+    var that = this;
     console.log(e.detail.value.input)
     var val = e.detail.value.input ;
     wx.request({
-      url: 'http://apis.juhe.cn',
+      url: 'http://apis.juhe.cn/idcard/index',
       data: {
           key:'bcc2fe431500a3db116b39857baf0883',
           cardno:val
       },
       method: 'GET', 
       success: function(res){
-        console.log(res)
+        console.log(res.data)
+        that.setData({
+          area:res.data.result.area,
+          birthday:res.data.result.birthday
+        })
       },
       fail: function() {
         // fail
