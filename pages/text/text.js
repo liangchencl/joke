7dc16e3441d2b28513bfbf64bc9d5f6f3d6e0f06
凉城 <627150799@qq.com>
2016-12-17 17:21:47 +0800

    1024随机生成数字，位置

--- pages/text/text.js
+++ pages/text/text.js
@@ -1,40 +1,82 @@
 // pages/text/text.js
+var startLeft;
+var startTop;
+var endLeft;
+var endTop;
+// 随机生成2个位置用于放置数字
+var radom = function(that,num1,num2,num3,num4){
+    var item_0_0_left=[0,120,240,360];
+    var item_0_0_top=[0,120,240,360];
+    var item_top = item_0_0_top[num1];
+    var item_left = item_0_0_left[num2];
+    var item_top2 = item_0_0_top[num3];
+    var item_left2 = item_0_0_left[num4];
+    that.setData({
+      top:item_top,
+      left:item_left,
+      top2:item_top2,
+      left2:item_left2
+    })
+};
+
 Page({
-  data:{},
+  data:{
+    item_0_0_left:[0,120,240,360],
+    item_0_0_top:[0,120,240,360]
+  },
   onLoad:function(e){
     var that = this;
-    // 页面初始化 options为页面跳转所带来的参数
-    for(var i=0;i<4;i++){
-      for(var j=0;j<4;j++){
-        var item_left = 120*i;
-        var item_top = 120*j;
-        console.log(item_top)
-        that.setData({
-          item_0_0_left:item_left,
-          item_0_0_right:item_top
-        })
-      }
-    }
+    // 
+    var num1 = Math.floor(Math.random()*4);
+    var num2 = Math.floor(Math.random()*4);
+    var num3 = Math.floor(Math.random()*4);
+    var num4 = Math.floor(Math.random()*4);
+
+    if(num1==num3 && num2==num4){
+      var num1 = Math.floor(Math.random()*4);
+      var num2 = Math.floor(Math.random()*4);
+      radom(that,num1,num2,num3,num4)
+    }else{
+      radom(that,num1,num2,num3,num4)
+    };
+    // 随机生成两个数字 2 或者 4 
+    var n1 = Math.random() < 0.5 ? 2 : 4;
+    var n2 = Math.random() < 0.5 ? 2 : 4;
+    that.setData({
+      n1:n1,
+      n2:n2
+    })    
   },
-  onReady:function(){
-    // 页面渲染完成
-  },
-  onShow:function(){
-    // 页面显示
-  },
-  onHide:function(){
-    // 页面隐藏
-  },
-  onUnload:function(){
-    // 页面关闭
-  },
+
   startFirst:function(e){
-    console.log("kaishi")
+    startLeft = e.changedTouches[0].clientX;
+    startTop = e.changedTouches[0].clientY;
   },
   moveFirst:function(e){
     console.log("move")
   },
   endFirst:function(e){
-    console.log("end")
+    endLeft = e.changedTouches[0].clientX;
+    endTop = e.changedTouches[0].clientY;
+
+    console.log(startLeft)
+    console.log(endLeft)
+    console.log('///////////')
+    
+    console.log(startTop)
+    console.log(endTop)
+
+    if(endLeft - startLeft > 0 && endLeft - startLeft > Math.abs(startTop - endTop)){
+      console.log("向右移动了")
+
+    }else if(endLeft - startLeft < 0 && startLeft - endLeft > Math.abs(endTop - startTop) ){
+      console.log("向左移动了")
+
+    }else if( startTop - endTop > 0 && startTop - endTop > Math.abs(endLeft - startLeft)){
+      console.log("向上移动了")
+      
+    }else if( endTop - startTop > 0 && endTop - startTop > Math.abs(endLeft - startLeft)){
+      console.log("向下移动了")
+    }
   }
 })
