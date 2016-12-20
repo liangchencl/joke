f4055326dab67dadf2d04cdb75d2e98dedf65412
凉城 <627150799@qq.com>
2016-12-20 15:30:45 +0800

    11

--- pages/map/map.js
+++ pages/map/map.js
@@ -21,8 +21,46 @@ Page({
   },
   
   onLoad:function(options){
+    var that = this;
     // 页面初始化 options为页面跳转所带来的参数
-    
+    wx.getLocation({
+      type: 'gcj02', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标
+      success: function(res){
+        // success
+        var latitude = res.latitude
+        var longitude = res.longitude
+        
+        console.log(latitude)
+        console.log(longitude)
+        that.setData({
+          latitude : latitude,
+          longitude : longitude,
+          markers:[{
+            latitude:latitude,
+            longitude : longitude,
+            name: '科兴科学园',
+            desc: '位置貌似定的不对'
+          }],
+          covers: [{
+            latitude: latitude,
+            longitude: longitude,
+            iconPath: '../images/car.png',
+            rotate: 10
+          }, {
+            latitude: latitude,
+            longitude: longitude,
+            iconPath: '../images/car.png',
+            rotate: 90
+          }]
+        })
+      },
+      fail: function() {
+        // fail
+      },
+      complete: function() {
+        // complete
+      }
+    })
   },
   onReady:function(){
     // 页面渲染完成
